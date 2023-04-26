0829354ad311766780898724b1ba3de5bbade6c5
--Ivan Luna <--IvanLuna2101@hotmail.com>
2023-04-25 18:01:55 -0600

    Cambios del fondo

--- js/Inicio.js
+++ js/Inicio.js
@@ -114,3 +114,20 @@ function actualizaInfo(maximoCaracteres){
         info.innerHTML = "Puedes escribir hasta " + (maximoCaracteres - elemento.value.length) + " caracteres adicionales";
     }
 }
+
+// Selecciona todas las celdas de fecha
+const dateCells = document.querySelectorAll('td[data-datetime]');
+
+// Loop a través de cada celda de fecha
+dateCells.forEach((cell) => {
+
+// Agrega un listener de clic a cada celda
+cell.addEventListener('click', () => {
+
+// Obtener la fecha y hora de la celda
+const datetime = cell.dataset.datetime;
+
+// Mostrar la fecha y hora en una ventana de alerta
+alert(`Has seleccionado la fecha y hora: ${datetime}`);
+});
+});
